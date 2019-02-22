0f3995ab5cd1eeb9dc31721cffcf4895e3ffa4cb
tsaraya29 <saratgrffn@gmail.com>
2019-02-21 22:06:19 -0600

    working but buggy

--- main.js
+++ main.js
@@ -1,15 +1,59 @@
 console.log("This is loaded");
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCK3f1JgheHr3Zc99VoekTw1vzt9aoULw0",
-    authDomain: "firstfirebase-tsaraya29.firebaseapp.com",
-    databaseURL: "https://firstfirebase-tsaraya29.firebaseio.com",
-    projectId: "firstfirebase-tsaraya29",
-    storageBucket: "firstfirebase-tsaraya29.appspot.com",
-    messagingSenderId: "1084637313008"
-  };
-  firebase.initializeApp(config);
+var db = firebase.database();
 
-  //https://firebase.google.com/docs/web/setup?authuser=0
-  // /https://console.firebase.google.com/u/0/project/firstfirebase-tsaraya29/settings/general/
+var train = "";
+var destination = "";
+var firstTrain = 0;
+var freq = 0;
+var currentTime = moment();
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+  train = $("#name").val().trim();
+  destination = $("#destination").val().trim();
+  firstTrain = $("#firstTrain").val().trim();
+  freq = $("#freq").val().trim();  
+
+
+//clear entries
+$("#name").val("");
+$("#destination").val("");
+$("#firstTrain").val("");
+$("#freq").val("");
+
+setInterval(function(){
+  $("#current-time").html(moment(moment()).format("hh:mm:ss"));
+}, 1000);
+
+// add to firebase
+db.ref().push({
+  train: train,
+  destination: destination,
+  firstTrain: firstTrain,
+  freq: freq
+});
+
+db.ref().on("child_added", function(param) {
+
+  //calculations   
+    var firstTime = moment(param.val().firstTrain, "hh:mm").subtract(1, "days");
+  
+     var timeDiff = moment().diff(moment(firstTime), "minutes");
+      //console.log("Difference in time: " + timeDiff);
+  
+      var remainder = timeDiff % param.val().freq;
+      //console.log("Remainder: " + remainder);
+  
+      var minutesAway = param.val().freq - remainder;
+      //console.log("Time till Train: " + minsUntilTrain);
+  
+      var nextTrainTime = moment().add(minutesAway, "minutes");
+      //console.log("Next arrival: " + moment(nextTrainTime).format("hh:mm"));
+
+      $("#schedule > tbody").append("<tr><td>" + param.val().train+ 
+      "</td><td>" + param.val().destination + 
+      "</td><td>" + param.val().freq + 
+      "</td><td>" + moment(nextTrainTime).format("hh:mm") + "</td><td>" + minutesAway + " Minutes</td></tr>");
+      });
+    });
